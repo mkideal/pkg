661cb93fc6780165a63db6087566e0eda45fa808
mkideal <i@mkideal.com>
2016-11-05 13:19:22 +0800

    add redisorm

---
+++ redisorm/ormdis_test.go
@@ -0,0 +1,228 @@
+package orm_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/mkideal/log"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"live_common_lib/qqredis/orm"
+	"live_common_lib/typeconv"
+)
+
+var (
+	redisc *mockRedisclient
+
+	errArgumentsLength = errors.New("invalid arguments length")
+	errUnknownField    = errors.New("unknown field")
+)
+
+func init() {
+	redisc = &mockRedisclient{
+		tables: map[string]mockTable{},
+	}
+}
+
+type mockTable map[string]interface{}
+
+func (t mockTable) set(field string, value interface{}) {
+	t[field] = value
+}
+
+func (t mockTable) get(field string) (string, bool) {
+	value, found := t[field]
+	return orm.ToString(value), found
+}
+
+func (t mockTable) del(field string) bool {
+	if _, found := t[field]; found {
+		delete(t, field)
+		return true
+	}
+	return false
+}
+
+type mockRedisclient struct {
+	tables map[string]mockTable
+}
+
+func (c *mockRedisclient) getTable(name string) mockTable {
+	table, ok := c.tables[name]
+	if ok {
+		return table
+	}
+	table = mockTable{}
+	c.tables[name] = table
+	return table
+}
+
+func (c *mockRedisclient) HsetMulti(args ...interface{}) (string, error) {
+	if len(args)%2 == 0 || len(args) == 1 {
+		return "", errArgumentsLength
+	}
+	tableName := orm.ToString(args[0])
+	table := c.getTable(tableName)
+	for i := 1; i+1 < len(args); i += 2 {
+		table.set(orm.ToString(args[i]), args[i+1])
+	}
+	return orm.ToString(len(args) / 2), nil
+}
+
+func (c *mockRedisclient) Hmgetstrings(args ...interface{}) (int, []*string, error) {
+	if len(args) < 2 {
+		return 0, nil, errArgumentsLength
+	}
+	values := make([]*string, 0, len(args)-1)
+	tableName := orm.ToString(args[0])
+	table := c.getTable(tableName)
+	n := 0
+	for i := 1; i < len(args); i++ {
+		field := orm.ToString(args[i])
+		value, found := table.get(field)
+		if found {
+			values = append(values, &value)
+		} else {
+			values = append(values, nil)
+			n++
+		}
+	}
+	return n, values, nil
+}
+
+func (c *mockRedisclient) Hdel(args ...interface{}) (string, error) {
+	if len(args) < 2 {
+		return "0", errArgumentsLength
+	}
+	tableName := orm.ToString(args[0])
+	table := c.getTable(tableName)
+	n := 0
+	for i := 1; i < len(args); i++ {
+		field := orm.ToString(args[i])
+		if table.del(field) {
+			n++
+		}
+	}
+	return orm.ToString(n), nil
+}
+
+func TestOrm(t *testing.T) {
+	defer log.Uninit(log.InitConsole(log.LvFATAL))
+
+	eng := orm.NewEngine("test", redisc)
+	eng.SetErrorHandler(func(action string, err error) error {
+		log.Printf(orm.ErrorHnadlerDepth, log.LvWARN, "<%s>: %v", action, err)
+		return err
+	})
+
+	// Insert
+	inserted := &User{Id: 1, Name: "test1", Age: 10}
+	eng.Insert(inserted)
+	t.Logf("insert 1: %v", redisc.tables)
+
+	// Get
+	loaded := &User{Id: 1}
+	eng.Get(loaded)
+	assert.Equal(t, inserted.Id, loaded.Id)
+	assert.Equal(t, inserted.Name, loaded.Name)
+	assert.Equal(t, inserted.Age, loaded.Age)
+
+	// Update
+	inserted.Age = 20
+	eng.Update(inserted, "age")
+	loaded = &User{Id: 1}
+	eng.Get(loaded)
+	assert.Equal(t, inserted.Id, loaded.Id)
+	assert.Equal(t, inserted.Name, loaded.Name)
+	assert.Equal(t, inserted.Age, loaded.Age)
+	assert.Equal(t, 20, loaded.Age)
+
+	// Remove
+	eng.Remove(inserted.Meta(), inserted.Key())
+	found, err := eng.Get(&User{Id: 1})
+	assert.Nil(t, err)
+	assert.False(t, found)
+
+	users := []*User{
+		{Id: 1, Name: "test1", Age: 10},
+		{Id: 2, Name: "test2", Age: 20},
+		{Id: 3, Name: "test3", Age: 30},
+	}
+	keys := make([]int64, 0, len(users))
+	for _, u := range users {
+		eng.Insert(u)
+		keys = append(keys, u.Id)
+	}
+	t.Logf("insert %v: %v", keys, redisc.tables)
+
+	// Find
+	tf := newUserSlice(len(users))
+	eng.Find(userMeta, tf, orm.Int64Keys(keys))
+	require.Equal(t, len(users), len(tf.data))
+	for i := 0; i < len(users); i++ {
+		assert.Equal(t, users[i].Id, tf.data[i].Id)
+		assert.Equal(t, users[i].Name, tf.data[i].Name)
+		assert.Equal(t, users[i].Age, tf.data[i].Age)
+	}
+
+	// Error
+	loaded = &User{Id: 1}
+	err = eng.Update(loaded, "invalid_field")
+	require.NotNil(t, err)
+	t.Logf(err.Error())
+}
+
+type UserMeta struct{}
+
+var userMeta = UserMeta{}
+
+func (meta UserMeta) Name() string     { return "user" }
+func (meta UserMeta) Fields() []string { return []string{"name", "age"} }
+
+type userSlice struct {
+	data []User
+}
+
+func newUserSlice(cap int) *userSlice {
+	return &userSlice{
+		data: make([]User, 0, cap),
+	}
+}
+
+func (us *userSlice) New(key interface{}) orm.FieldSetter {
+	us.data = append(us.data, User{Id: key.(int64)})
+	return &us.data[len(us.data)-1]
+}
+
+type User struct {
+	Id   int64
+	Name string
+	Age  int
+}
+
+func (u User) Meta() orm.TableMeta { return userMeta }
+func (u User) Key() interface{}    { return u.Id }
+
+func (u User) GetField(field string) (interface{}, bool) {
+	switch field {
+	case "name":
+		return u.Name, true
+	case "age":
+		return u.Age, true
+	default:
+		return nil, false
+	}
+}
+
+func (u *User) SetField(field string, value string) error {
+	switch field {
+	case "name":
+		u.Name = value
+	case "age":
+		return typeconv.SetIntFromStr(&u.Age, value)
+	default:
+		return errUnknownField
+	}
+	return nil
+}
---
+++ redisorm/table.go
@@ -0,0 +1,103 @@
+package orm
+
+// KeyList holds n keys
+type KeyList interface {
+	Len() int
+	Key(int) interface{}
+}
+
+type IntKeys []int
+type Int64Keys []int64
+type Uint64Keys []uint64
+type StringKeys []string
+type InterfaceKeys []interface{}
+
+func (keys IntKeys) Len() int       { return len(keys) }
+func (keys Int64Keys) Len() int     { return len(keys) }
+func (keys Uint64Keys) Len() int    { return len(keys) }
+func (keys StringKeys) Len() int    { return len(keys) }
+func (keys InterfaceKeys) Len() int { return len(keys) }
+
+func (keys IntKeys) Key(i int) interface{}       { return keys[i] }
+func (keys Int64Keys) Key(i int) interface{}     { return keys[i] }
+func (keys Uint64Keys) Key(i int) interface{}    { return keys[i] }
+func (keys StringKeys) Key(i int) interface{}    { return keys[i] }
+func (keys InterfaceKeys) Key(i int) interface{} { return keys[i] }
+
+// FieldList holds n fields
+type FieldList interface {
+	Len() int
+	Field(int) string
+}
+
+// Field implements FieldList which atmost contains one value
+type Field string
+
+func (f Field) Len() int {
+	if f == "" {
+		return 0
+	}
+	return 1
+}
+
+func (f Field) Field(i int) string { return string(f) }
+
+// Fields implements FieldList
+type FieldSlice []string
+
+func (fs FieldSlice) Len() int           { return len(fs) }
+func (fs FieldSlice) Field(i int) string { return fs[i] }
+
+type Index interface {
+	Name() string
+	RefTable() string
+	Update(member interface{}, score int64) error
+}
+
+//-----------------
+// Basic interface
+//-----------------
+
+type FieldGetter interface {
+	GetField(field string) (interface{}, bool)
+}
+
+type FieldSetter interface {
+	SetField(field string, value string) error
+}
+
+// TableMeta holds table meta information
+type TableMeta interface {
+	Name() string
+	Fields() []string
+}
+
+//-------------------
+// Compose interface
+//-------------------
+
+type TableInfo interface {
+	Meta() TableMeta
+	Key() interface{}
+}
+
+type ReadonlyTable interface {
+	TableInfo
+	FieldGetter
+}
+
+type WriteonlyTable interface {
+	TableInfo
+	FieldSetter
+}
+
+// Table represents a hash_table in redis
+type Table interface {
+	TableInfo
+	FieldGetter
+	FieldSetter
+}
+
+type FieldSetterFactory interface {
+	New(key interface{}) FieldSetter
+}
