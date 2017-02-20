f5fe57c7d939f238dd6586d04e61c3008fb9386e
mkideal <i@mkideal.com>
2017-02-20 13:43:26 +0800

    fix typrconv

--- netutil/httputil/httputil.go
+++ netutil/httputil/httputil.go
@@ -2,10 +2,76 @@ package httputil
 
 import (
 	"encoding/json"
+	"encoding/xml"
+	"errors"
 	"net/http"
 	"strings"
 
 	"github.com/mkideal/pkg/option"
+)
+
+var (
+	ErrUnableToMarshalForm = errors.New("unable to marshal form")
+)
+
+const (
+	charsetUTF8 = "charset=utf-8"
+
+	MIMEApplicationJSON                  = "application/json"
+	MIMEApplicationJSONCharsetUTF8       = MIMEApplicationJSON + "; " + charsetUTF8
+	MIMEApplicationJavaScript            = "application/javascript"
+	MIMEApplicationJavaScriptCharsetUTF8 = MIMEApplicationJavaScript + "; " + charsetUTF8
+	MIMEApplicationXML                   = "application/xml"
+	MIMEApplicationXMLCharsetUTF8        = MIMEApplicationXML + "; " + charsetUTF8
+	MIMEApplicationForm                  = "application/x-www-form-urlencoded"
+	MIMEApplicationProtobuf              = "application/protobuf"
+	MIMEApplicationMsgpack               = "application/msgpack"
+	MIMETextHTML                         = "text/html"
+	MIMETextHTMLCharsetUTF8              = MIMETextHTML + "; " + charsetUTF8
+	MIMETextPlain                        = "text/plain"
+	MIMETextPlainCharsetUTF8             = MIMETextPlain + "; " + charsetUTF8
+	MIMEMultipartForm                    = "multipart/form-data"
+	MIMEOctetStream                      = "application/octet-stream"
+)
+
+const (
+	HeaderAccept                        = "Accept"
+	HeaderAcceptEncoding                = "Accept-Encoding"
+	HeaderAllow                         = "Allow"
+	HeaderAuthorization                 = "Authorization"
+	HeaderContentDisposition            = "Content-Disposition"
+	HeaderContentEncoding               = "Content-Encoding"
+	HeaderContentLength                 = "Content-Length"
+	HeaderContentType                   = "Content-Type"
+	HeaderCookie                        = "Cookie"
+	HeaderSetCookie                     = "Set-Cookie"
+	HeaderIfModifiedSince               = "If-Modified-Since"
+	HeaderLastModified                  = "Last-Modified"
+	HeaderLocation                      = "Location"
+	HeaderUpgrade                       = "Upgrade"
+	HeaderVary                          = "Vary"
+	HeaderWWWAuthenticate               = "WWW-Authenticate"
+	HeaderXForwardedProto               = "X-Forwarded-Proto"
+	HeaderXHTTPMethodOverride           = "X-HTTP-Method-Override"
+	HeaderXForwardedFor                 = "X-Forwarded-For"
+	HeaderXRealIP                       = "X-Real-IP"
+	HeaderServer                        = "Server"
+	HeaderOrigin                        = "Origin"
+	HeaderAccessControlRequestMethod    = "Access-Control-Request-Method"
+	HeaderAccessControlRequestHeaders   = "Access-Control-Request-Headers"
+	HeaderAccessControlAllowOrigin      = "Access-Control-Allow-Origin"
+	HeaderAccessControlAllowMethods     = "Access-Control-Allow-Methods"
+	HeaderAccessControlAllowHeaders     = "Access-Control-Allow-Headers"
+	HeaderAccessControlAllowCredentials = "Access-Control-Allow-Credentials"
+	HeaderAccessControlExposeHeaders    = "Access-Control-Expose-Headers"
+	HeaderAccessControlMaxAge           = "Access-Control-Max-Age"
+
+	HeaderStrictTransportSecurity = "Strict-Transport-Security"
+	HeaderXContentTypeOptions     = "X-Content-Type-Options"
+	HeaderXXSSProtection          = "X-XSS-Protection"
+	HeaderXFrameOptions           = "X-Frame-Options"
+	HeaderContentSecurityPolicy   = "Content-Security-Policy"
+	HeaderXCSRFToken              = "X-CSRF-Token"
 )
 
 func IP(req *http.Request) string {
@@ -32,20 +98,70 @@ func getproxy(req *http.Request) []string {
 	return []string{}
 }
 
+func Response(w http.ResponseWriter, status int, acceptType string, value interface{}, debug ...bool) error {
+	switch {
+	case strings.HasPrefix(acceptType, MIMEApplicationXML):
+		return XMLResponse(w, status, value, debug...)
+	case strings.HasPrefix(acceptType, MIMEApplicationForm):
+		return FormResponse(w, status, value)
+	default:
+		return JSONResponse(w, status, value, debug...)
+	}
+}
+
 func JSONResponse(w http.ResponseWriter, status int, value interface{}, debug ...bool) error {
+	return ResponseWithMarshaler(w, status, MIMEApplicationJSONCharsetUTF8, value, json.Marshal, json.MarshalIndent, debug...)
+}
+
+func XMLResponse(w http.ResponseWriter, status int, value interface{}, debug ...bool) error {
+	return ResponseWithMarshaler(w, status, MIMEApplicationXMLCharsetUTF8, value, xml.Marshal, xml.MarshalIndent, debug...)
+}
+
+func FormResponse(w http.ResponseWriter, status int, value interface{}) error {
+	return ResponseWithMarshaler(w, status, MIMEApplicationForm, value, MarshalForm, nil)
+}
+
+func TextResponse(w http.ResponseWriter, status int, value string) error {
+	w.WriteHeader(status)
+	_, err := w.Write([]byte(value))
+	return err
+}
+
+type MarshalFunc func(interface{}) ([]byte, error)
+type MarshalIndentFunc func(interface{}, string, string) ([]byte, error)
+
+type FormMarshaler interface {
+	MarshalForm() ([]byte, error)
+}
+
+func MarshalForm(v interface{}) ([]byte, error) {
+	if marshaler, ok := v.(FormMarshaler); ok {
+		return marshaler.MarshalForm()
+	}
+	return nil, ErrUnableToMarshalForm
+}
+
+func ResponseWithMarshaler(
+	w http.ResponseWriter,
+	status int,
+	contentType string,
+	value interface{},
+	marshal MarshalFunc,
+	marshalIndent MarshalIndentFunc,
+	debug ...bool) error {
 	var (
 		b   []byte
 		err error
 	)
-	if option.Bool(false, debug...) {
-		b, err = json.MarshalIndent(value, "", "  ")
+	if option.Bool(false, debug...) && marshalIndent != nil {
+		b, err = marshalIndent(value, "", "  ")
 	} else {
-		b, err = json.Marshal(value)
+		b, err = marshal(value)
 	}
 	if err != nil {
 		return err
 	}
-	return BlobResponse(w, status, "application/json;charset=utf-8", b)
+	return BlobResponse(w, status, contentType, b)
 }
 
 func BlobResponse(w http.ResponseWriter, status int, contentType string, b []byte) error {
--- typeconv/typeconv.go
+++ typeconv/typeconv.go
@@ -1,10 +1,46 @@
 package typeconv
 
 import (
+	"fmt"
 	"strconv"
 )
 
 const Unused = 0
+
+func ToString(v interface{}) string {
+	switch value := v.(type) {
+	case int:
+		return strconv.FormatInt(int64(value), 10)
+	case int8:
+		return strconv.FormatInt(int64(value), 10)
+	case int16:
+		return strconv.FormatInt(int64(value), 10)
+	case int32:
+		return strconv.FormatInt(int64(value), 10)
+	case int64:
+		return strconv.FormatInt(int64(value), 10)
+	case uint:
+		return strconv.FormatUint(uint64(value), 10)
+	case uint8:
+		return strconv.FormatUint(uint64(value), 10)
+	case uint16:
+		return strconv.FormatUint(uint64(value), 10)
+	case uint32:
+		return strconv.FormatUint(uint64(value), 10)
+	case uint64:
+		return strconv.FormatUint(uint64(value), 10)
+	case float32:
+		return strconv.FormatFloat(float64(value), 'g', -1, 32)
+	case float64:
+		return strconv.FormatFloat(value, 'g', -1, 64)
+	case string:
+		return value
+	case []byte:
+		return string(value)
+	default:
+		return fmt.Sprintf("%v", v)
+	}
+}
 
 func String2Int(v *int, s string) error {
 	i, err := strconv.Atoi(s)
