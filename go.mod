73dad9e2b74e7760b31f80fdccf277fcdfdb5c77
mkideal <i@mkideal.com>
2020-07-11 00:32:37 +0800

    dd prompt

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/mattn/go-colorable v0.1.7
 	github.com/mattn/go-isatty v0.0.12
 	github.com/mkideal/log v1.0.0
+	github.com/stretchr/testify v1.4.0
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	golang.org/x/net v0.0.0-20200707034311-ab3426394381
 	gopkg.in/redis.v5 v5.2.9
 )
---
+++ prompt/prompt.go
@@ -0,0 +1,79 @@
+package command
+
+import (
+	"errors"
+	"io"
+	"os"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+var (
+	errRequiredMissing = errors.New("required missing")
+	errInvalidBoolean  = errors.New("invalid boolean")
+)
+
+type readerWriter struct {
+	io.Reader
+	io.Writer
+}
+
+func prompt(text string, password bool) (string, error) {
+	stdin := os.Stdin
+	stdout := os.Stdout
+	term := terminal.NewTerminal(readerWriter{stdin, stdout}, text)
+	stdinFD := int(stdin.Fd())
+	stdinState, err := terminal.MakeRaw(stdinFD)
+	if err != nil {
+		return "", err
+	}
+	defer terminal.Restore(stdinFD, stdinState)
+
+	var line string
+	if password {
+		line, err = term.ReadPassword(text)
+	} else {
+		line, err = term.ReadLine()
+	}
+	if err != nil {
+		return "", err
+	}
+	return line, nil
+}
+
+func Prompt(text string, required bool) (string, error) {
+	line, err := prompt(text, false)
+	if err != nil {
+		return line, err
+	}
+	if required && line == "" {
+		return line, errRequiredMissing
+	}
+	return line, err
+}
+
+func PromptDefault(text string, dft string) (string, error) {
+	line, err := prompt(text, false)
+	if err != nil {
+		return line, err
+	}
+	if line == "" {
+		return dft, nil
+	}
+	return line, err
+}
+
+func Password(text string) (string, error) {
+	return prompt(text, true)
+}
+
+func Ask(question string, dft bool) (bool, error) {
+	line, err := prompt(question, false)
+	if err != nil {
+		return false, err
+	}
+	if line == "" {
+		return dft, nil
+	}
+	return line == "y" || line == "Y" || line == "yes" || line == "Yes" || line == "YES", nil
+}
